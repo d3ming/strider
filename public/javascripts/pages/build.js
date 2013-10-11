fe5a901999720d79e468c03e7a090d8b122d9983
Dong Ming <dong.ming@paperg.com>
2013-10-10 17:34:17 -0700

    wtf?

--- public/javascripts/pages/build.js
+++ public/javascripts/pages/build.js
@@ -391,6 +391,7 @@ app.controller('JobCtrl', ['$scope', '$route', '$location', 'jobs', function ($s
     if ($scope.job.status === 'running' ||
       $scope.job.status === 'submitted') return;
     // TODO: URL encode and other good things
+    ////var gitHubCompareUrl = "https://github.com/" + job.repo_owner + "/" + job.repo_name + "/compare/" + job.repo_name + ":master..." + job.repo_owner + ":master";
     var gitHubCompareUrl = "https://github.com/" + job.repo_owner + "/" + job.repo_name + "/compare/" + job.repo_name + ":master..." + job.repo_owner + ":master";
     window.open(gitHubCompareUrl);
     $scope.job = {
