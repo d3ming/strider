2ecff370033c329202d6124f5a562e2db1d44fdc
Dong Ming <dong.ming@paperg.com>
2013-10-10 17:13:06 -0700

    Testing

--- public/javascripts/pages/build.js
+++ public/javascripts/pages/build.js
@@ -386,11 +386,15 @@ app.controller('JobCtrl', ['$scope', '$route', '$location', 'jobs', function ($s
       status: 'submitted',
       output: ''
     };
-  $scope.startReview = function () {
-    if ($scope.job.status === 'running' ||
-      $scope.job.status === 'submitted') return;
-    window.open("http://www.github.com");
-    };
+    $scope.startReview = function () {
+      if ($scope.job.status === 'running' ||
+        $scope.job.status === 'submitted') return;
+      startJob($scope.job.repo_url, 'TEST_AND_DEPLOY');
+      $scope.job = {
+        repo_url: $scope.job.repo_url,
+        status: 'submitted',
+        output: ''
+      };
   };
 
   // Socket update stuff
